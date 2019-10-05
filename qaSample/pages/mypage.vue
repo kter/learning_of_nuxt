a833829c7f94ac748dcfdffd84214e078fe0ec10
Takahashi Tomohiko <takahashi@tomohiko.io>
2019-10-05 19:46:13 +0900

    マイページを追加

---
+++ qaSample/pages/mypage.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <div>
+        <section class="section no-top-pad">
+            <h5 class="title is-5">マイページ</h5>
+            <hr />
+
+            <div class="is-centered is-mobile">
+                <div class="box">
+                    <p>{{user.name}}</p>
+                    <p>{{user.email}}</p>
+                </div>
+                <p class="button is-outlined" @click="logOut">ログアウト</p>
+            </div>
+        </section>
+    </div>
+</template>
