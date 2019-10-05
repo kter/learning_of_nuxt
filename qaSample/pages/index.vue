b5c6dd01b09a9eaf6aa694cb76db92cf39afaafe
Takahashi Tomohiko <takahashi@tomohiko.io>
2019-10-05 21:06:47 +0900

    トップページに質問投稿を追加

--- qaSample/pages/index.vue
+++ qaSample/pages/index.vue
@@ -1,72 +1,56 @@
-<template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        qaSample
-      </h1>
-      <h2 class="subtitle">
-        My smashing Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
-</template>
-
 <script>
-import Logo from '~/components/Logo.vue'
-
+import apiJobMixin from "@/mixins/apiJobMixin";
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      question: "",
+    };
+  },
+  mixins: [apiJobMixin],
+  methods: {
+    onQuestion() {
+      let userID = this.$store.getters.user.id;
+      const payload = {
+        question: this.question,
+        userId: userID
+      };
+      this.$store.dispatch("question/addQuestion", payload);
+    },
+    jobsDone() {
+      console.log("job done");
+    }
   }
-}
+};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<template>
+  <div>
+    <section class="section no-top-pad">
+      <div class="columns is-centered is-mobile">
+        <div v-if="isLogin" class="column is-half-desktop is-full-mobile is-full-tablet">
+          <form @submit.prevent="onQuestion">
+            <div class="field">
+              <label class="label">あなたの質問は？</label>
+              <div class="control">
+                <textarea class="textarea" v-model="question" placeholder="質問を入力して下さい"></textarea>
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <button
+                  class="button is-primary"
+                  :class="{ 'is-loading': busy }"
+                  :disabled="busy"
+                >質問する</button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div v-else>アカウントを作成して質問をしましょう！</div>
+      </div>
+      <div class="is-centered is-mobile">
+        <!-- ここに質問リスト -->
+      </div>
+    </section>
+  </div>
+</template>
