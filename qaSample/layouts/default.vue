adc1f809a45a33565cb04dac8224d1dd54e90274
Takahashi Tomohiko <takahashi@tomohiko.io>
2019-10-05 19:32:19 +0900

    レイアウトにログイン処理を追加

--- qaSample/layouts/default.vue
+++ qaSample/layouts/default.vue
@@ -1,3 +1,42 @@
+<script>
+import { mapMutations } from "vuex";
+export default {
+  created() {
+    // pluginsでinjectしたfirebaseを呼び出す
+    // onAuthStateChangedでログイン状態を判定
+    this.$fireApp.auth().onAuthStateChanged(user => {
+      if (user) {
+        const authUser = {
+          id: user.id,
+          email: user.email,
+          name: user.displayName
+        };
+      }
+    });
+  },
+  computed: {
+    isLogin() {
+      // テンプレート側でログイン状態を判定したいのでcomputedを作成
+      const loggedIn = this.$store.getters.loginStatus;
+      if (loggedIn) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    userName() {
+      // storeからのユーザーネーム取得処理
+      return this.$store.getters.user ? this.$store.getters.user.name : "";
+    }
+  },
+  methods: {
+    ...mapMutations({
+      setUser: "setUser"
+    })
+  }
+};
+</script>
+
 <template>
   <div>
     <nav class="navbar" role="navigation" aria-label="main navigation">
@@ -14,7 +53,11 @@
         <div class="navbar-end">
           <div class="navbar-item">
             <div class="buttons">
-              <!-- ここにログインボタン -->
+              <nuxt-link v-if="!isLogin" class="button is-light" to="/login">ログイン</nuxt-link>
+              <nuxt-link v-else class="button is-light" to="/mypage">{{userName}}さん</nuxt-link>
+              <nuxt-link class="button is-primary" to="/signup">
+                <strong>アカウント作成</strong>
+              </nuxt-link>
             </div>
           </div>
         </div>
